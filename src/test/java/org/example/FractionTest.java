11696a11e7ed60b80b67d446b5f4b89b030f72b2
ereining <ereining@htl-steyr.ac.at>
2022-10-05 09:34:43 +0200

    test push

--- src/test/java/org/example/FractionTest.java
+++ src/test/java/org/example/FractionTest.java
@@ -75,7 +75,7 @@ class FractionTest {
         Fraction f3 = new Fraction(1, 0);
         Assertions.assertEquals(1, f3.getDivisor());
     }
-    //test
+
     @org.junit.jupiter.api.Test
     void setDivisor() {
         Fraction f = new Fraction(1, 10);
